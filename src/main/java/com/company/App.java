e9e0593de5d3a81a29302200ad84905626e1d039
Manisai9 <manisaidevasani@gmail.com>
2021-09-01 17:24:01 +0530

    Modified App.java

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -9,5 +9,6 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println(WordUtils.capitalize("hello world!"));
+        System.out.println(WordUtils.capitalize("hello world!"));
     }
 }
