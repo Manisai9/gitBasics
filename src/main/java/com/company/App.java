4ef6b0bdc9762a67c1d2772ee85e4610ab33efaa
Manisai Devasani <manisai@nbos.vinisoft.com>
2021-09-01 15:14:53 +0530

    Initial Commit

---
+++ src/main/java/com/company/App.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.apache.commons.text.WordUtils;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(WordUtils.capitalize("hello world!"));
+    }
+}
